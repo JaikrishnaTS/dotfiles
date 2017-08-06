5c5b6422c95b99588e57001ae8c3902d30dcfc99
Jaikrishna Tanjore Sukumar <t.s.jaikrishna@gmail.com>
2017-08-06 18:25:00 -0400

    Correct repo name

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ## Setup bash
-wget https://raw.githubusercontent.com/JaikrishnaTS/vim-config/master/.bash_profile -O $HOME/.bash_profile
+wget https://raw.githubusercontent.com/JaikrishnaTS/dotfiles/master/.bash_profile -O $HOME/.bash_profile
 
 ## Setup vim
 # download .vimrc file
-wget https://raw.githubusercontent.com/JaikrishnaTS/vim-config/master/.vimrc -O $HOME/.vimrc
+wget https://raw.githubusercontent.com/JaikrishnaTS/dotfiles/master/.vimrc -O $HOME/.vimrc
 
 # create dirs
 mkdir -p $HOME/.vim/colors $HOME/.vim/backup $HOME/.vim/swp $HOME/.vim/autoload
@@ -24,7 +24,7 @@ fi
 
 ## Setup tmux
 # download tmux conf
-wget https://raw.githubusercontent.com/JaikrishnaTS/vim-config/master/.tmux.conf -O $HOME/.tmux.conf
+wget https://raw.githubusercontent.com/JaikrishnaTS/dotfiles/master/.tmux.conf -O $HOME/.tmux.conf
 
 # create dirs
 mkdir -p $HOME/.tmux/plugins
